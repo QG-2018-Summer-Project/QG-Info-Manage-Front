3dc918ec3aea50297ebdf3a9bd9d6acda764badd
bobo1967 <1215373008@qq.com>
2018-08-19 16:14:49 +0800

    2

---
+++ srcipts/index.js
@@ -0,0 +1,65 @@
+/**
+ * 显示右面板功能
+ */
+(function() {
+    var showPanelButton = document.getElementById('show-panel-button'),
+        text = showPanelButton.getElementsByTagName('span')[0],
+        partRight = document.getElementsByClassName('part-right')[0];
+
+    EventUtil.addHandler(showPanelButton, 'click', function() {
+        if (ClassUtil.hasClass(partRight, 'show-part-right')) {
+            ClassUtil.removeClass(partRight, 'show-part-right');
+            text.innerHTML = '筛选条件';
+        } else {
+            ClassUtil.addClass(partRight, 'show-part-right');
+            text.innerHTML = '收起';
+        }
+    });
+})();
+
+/**
+ * 点击顶部导航栏
+ */
+(function() {
+    var headerLi = document.getElementsByClassName('header-ul')[0].getElementsByTagName('li');
+        firstMenu = document.getElementsByClassName('first-menu')[0];
+
+    for (let i = 0; i < headerLi.length; i++) {
+        EventUtil.addHandler(headerLi[i], 'click', callback);
+    }
+    function callback(e) {
+       createLi(e.target.innerHTML);
+        
+        // if (firstMenu.contains()) {
+        //     alert();
+        // }
+    }
+    /**
+     * 创建一个列表顶
+     * @param {string} content 
+     */
+    function createLi(content) {
+        var docFragment = document.createDocumentFragment(),
+            li = document.createElement('li'),
+            a = document.createElement('a'),
+            button = document.createElement('button'),
+            text = document.createTextNode(content);
+
+        a.setAttribute('href', 'javascript:');
+        button.setAttribute('class', 'close-button');
+        a.appendChild(text);
+        li.appendChild(a);
+        li.appendChild(button);
+        docFragment.appendChild(li);
+        firstMenu.appendChild(docFragment);
+        EventUtil.addHandler(button, 'click', deleteLi);
+    } 
+    /**
+     * 删除菜单选项
+     * @param {Event} e 
+     */
+    function deleteLi(e) {
+        console.log(e.target.parentNode);
+        firstMenu.removeChild(e.target.parentNode);
+    }
+})();
